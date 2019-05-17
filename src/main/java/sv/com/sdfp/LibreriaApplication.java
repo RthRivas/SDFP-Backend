48e67b0b0119e8848fe55f8e48edfbb35da49dcc
sagas1416 <sagastizado95@gmail.com>
2019-05-16 23:04:17 -0600

    Base de datos

--- src/main/java/sv/com/sdfp/LibreriaApplication.java
+++ src/main/java/sv/com/sdfp/LibreriaApplication.java
@@ -21,11 +21,27 @@ public class LibreriaApplication {
 	public CommandLineRunner setup(IProductosService produc) 
 	{
 		return (args) -> {
-			produc.save(new Productos("Libro",2,19.99,1,1,"http"));
-			produc.save(new Productos("vaca lola",5,12.87,1,1,"http"));
-			produc.save(new Productos("cepitillo",10,17.99,1,1,"http"));
-			produc.save(new Productos("Blaca nieve",14,18.99,1,1,"http"));
-			produc.save(new Productos("Aprendiendo matematicas",28,12.99,1,1,"http"));
+			produc.save(new Productos("colores Facela",10,3.75,1,1,"https://raw.githubusercontent.com/RthRivas/SDFP-Backend/master/src/assets/img/productos/estudiantes/colores.png"));
+			produc.save(new Productos("Compas",45,1.25,1,1,"http"));
+			produc.save(new Productos("Cuaderno rayado",250,1.25,1,1,"http"));			produc.save(new Productos("Blaca nieve",14,18.99,5,1,"http"));
+			produc.save(new Productos("Lapicero",500,0.20,1,1,"http"));
+			produc.save(new Productos("Lapiz",500,0.20,5,1,"http"));
+			produc.save(new Productos("Estuche de geometria",150,3.50,1,1,"http"));
+			produc.save(new Productos("Clip",350,0.02,1,2,"http"));
+			produc.save(new Productos("Engrapadora",200,2.50,1,2,"http"));
+			produc.save(new Productos("Perforadora",150,3.75,1,2,"http"));
+			produc.save(new Productos("Tijera",150,1.75,1,2,"http"));
+			produc.save(new Productos("Folder",500,0.20,1,3,"http"));
+			produc.save(new Productos("Resma de papel bond",700,5.00,1,3,"http"));
+			produc.save(new Productos("Papel Bond Color",350,0.10,1,3,"http"));
+			produc.save(new Productos("Cartulina",600,0.20,2,3,"http"));
+			produc.save(new Productos("Paginas Cuadriculadas",350,0.05,2,3,"http"));
+			produc.save(new Productos("EL CIPITÍO",50,15.00,3,4,"http"));
+			produc.save(new Productos("Alicia en el pais de las maravillas",35,12.50,3,4,"http"));
+			produc.save(new Productos("La estrategia oculta",20,25.00,3,4,"http"));
+			produc.save(new Productos("El Principito",35,10.00,3,4,"http"));
+			produc.save(new Productos("Ciudades de papel",10,55.00,3,4,"http"));
+			
 			
 		};
 	}
